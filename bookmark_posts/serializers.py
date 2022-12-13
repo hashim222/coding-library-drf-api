ee034c52658669672c30ae87618eef145a485f32
hashim222 <aslamhashim19@gmail.com>
2022-12-13 11:31:35 +0000

    created BookmarkSerilizer

---
+++ bookmark_posts/serializers.py
@@ -0,0 +1,28 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Bookmark
+
+
+# Help was taken from Code Institute's DRF API walkthrough project.
+class BookmarkSerializer(serializers.ModelSerializer):
+    '''
+    Bookmark serializer to converts Bookmark models into JSON,
+    which can be used for frontend.
+    '''
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Bookmark
+        fields = ['id', 'owner', 'post', 'created_on']
+
+    def create(self, validated_data):
+        '''
+        If a user tries to Bookmark the same post multiple times,
+        it will throw a duplicate error.
+        '''
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplication'
+            })
