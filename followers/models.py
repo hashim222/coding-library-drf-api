73b95ded7201055e77ecc15921344718a2fe2eae
hashim222 <aslamhashim19@gmail.com>
2022-12-14 12:44:04 +0000

    created Follower models

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Follower(models.Model):
+    '''
+    Follower model.
+    Users can follow each other.
+    '''
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='followed')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+        unique_together = ['owner', 'followed']
+
+    def __str__(self):
+        return f'Owner: {self.owner} - Followed User: {self.followed}'
