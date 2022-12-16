842f2650dd49853f004d680da8210d9913f4189e
hashim222 <aslamhashim19@gmail.com>
2022-12-16 13:01:31 +0000

    created post filter fields

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, permissions, filters
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import PostSerializer
 from .models import Post
 from coding_library.permissions import IsOwnerOrReadOnly
@@ -20,7 +21,8 @@ class PostList(generics.ListCreateAPIView):
 
     filter_backends = [
         filters.OrderingFilter,
-        filters.SearchFilter
+        filters.SearchFilter,
+        DjangoFilterBackend
     ]
 
     search_fields = [
@@ -34,6 +36,12 @@ class PostList(generics.ListCreateAPIView):
         'bookmark_count',
         'likes__created_on',
         'bookmark_posts__created_on'
+    ]
+
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'bookmark_posts__owner__profile',
+        'owner__profile'
     ]
 
     def perform_create(self, serializer):
